8768332d82fc2a5665c173cbfce12433fa7d4c1a
gjf2a <ferrer@hendrix.edu>
2024-08-10 19:53:07 -0500

    Ran cargo fmt.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
-use std::{
-    collections::VecDeque,
-    sync::Arc,
-    time::Instant,
-};
+use std::{collections::VecDeque, sync::Arc, time::Instant};
 
 use crossbeam_queue::SegQueue;
 use midi_msg::{MidiMsg, SystemRealTimeMsg};
